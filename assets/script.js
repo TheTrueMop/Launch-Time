98b799064e4a9074e837cffc18c91761dce0abf6
Dusticcus <dasafady@gmail.com>
2022-07-08 10:35:31 -0500

    API wired up

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,41 @@
-var appid = '&key=X2BCVEUMVC22RSDXLPE88U4YL';
+// Weather API Endpoints
+// There are four web service endpoints available:
 
-// var requestURL = 'https://api.spacexdata.com/v4/rockets/5e9d0d95eda69955f709d1eb';
+// /timeline – provides continuous weather data from historical observations, weather forecast and future statistical forecast based on previous conditions.
+// /forecast – provides access to up to 15-days of weather forecast information
+// /history – provides access to hourly and daily historical weather records
+// /historysummary – provides access to historical weather reports and processed information
 
-var previousLaunchRequestURL = "https://lldev.thespacedevs.com/2.2.0/launch/previous/?limit=200&offset=30"
+// var weatherBaseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/";
+var weatherAppid = '&key=X2BCVEUMVC22RSDXLPE88U4YL';
+
+var lat;
+var long;
+var launchDayTemp;
+var launchDate = "2022-07-01";
+
+function weatherForecast() {
+    var weatherBaseURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat}%2C%20${long}/${launchDate}?unitGroup=us&include=days&key=X2BCVEUMVC22RSDXLPE88U4YL&contentType=json`;
+    fetch(weatherBaseURL, {
+        method: 'GET', //GET is the default.
+        credentials: 'same-origin', // include, *same-origin, omit
+        redirect: 'follow', // manual, *follow, error
+    })
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            var theWeather = data;
+            launchDayTemp = data.days[0].temp;
+            console.log("Weather:");
+            console.log(launchDayTemp);
+        });
+}
+
+
+var missions;
+var missionsArray = [];
+var previousLaunchRequestURL = "https://lldev.thespacedevs.com/2.2.0/launch/previous/";
+var futureLaunchRequestURL = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/";
 
 function getPreviousLaunches() {
     fetch(previousLaunchRequestURL, {
@@ -10,10 +43,50 @@ function getPreviousLaunches() {
         credentials: 'same-origin', // include, *same-origin, omit
         redirect: 'follow', // manual, *follow, error
     })
-    .then(function (response) {
-    return response.json();
-    }).then(function(data) {
-    console.log(data);
-    });
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            missionsArray.push(JSON.stringify(data));
+            missions = JSON.parse(missionsArray);
+            console.log(missions);
+
+            for (let i = 0; i < 5; i++) {
+                lat = missions.results[i].pad.latitude;
+                long = missions.results[i].pad.longitude;
+
+                var item = document.createElement('li');
+                var img = document.createElement('img');
+                img.classList.add('shipImages');
+                img.src=missions.results[i].image;
+                var text = document.createTextNode(missions.results[i].name + " | " + missions.results[i].launch_service_provider.name);
+                item.appendChild(text);
+                document.getElementById('lastFiveLaunchesList').append(item);
+                item.appendChild(img);
+            }
+        });
 }
 getPreviousLaunches();
+
+// function getFutureLaunches() {
+//     fetch(futureLaunchRequestURL, {
+//         method: 'GET', //GET is the default.
+//         credentials: 'same-origin', // include, *same-origin, omit
+//         redirect: 'follow', // manual, *follow, error
+//     })
+//         .then(function (response) {
+//             return response.json();
+//         }).then(function (data) {
+//             console.log("Future:");
+//             console.log(data);
+//             missions = data;
+//             for (i = 0; i < 5; i++) {
+//                 var item = document.createElement('li');
+//                 var text = document.createTextNode(missions.results[i].name);
+//                 item.appendChild(text);
+//                 document.getElementById('nextFiveLaunchesList').append(item);
+//                 console.log();
+//             }
+//         });
+// }
+// getFutureLaunches();
+
