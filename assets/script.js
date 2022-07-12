6241664da531c6b02345481f51011f394270bd99
Myryitzy <104706728+Itzelmariana@users.noreply.github.com>
2022-07-12 12:35:08 -0500

    fixing bugs in modal for search page
    -Added modal into button
    -Show only one content selection

--- assets/script.js
+++ assets/script.js
@@ -58,13 +58,12 @@ function storeUniqueDataID() {
     localStorage.setItem("savedMissions", savedMissions);
     writeFutureMissionsToDom();
     addFavoriteToList();
-
   } else {
     savedMissions.push(dataID);
     localStorage.setItem("savedMissions", savedMissions);
     console.log("ID SAVED FROM FUTURE LAUNCHES");
     // console.log('ID SAVED FROM FUTURE LAUNCHES');
-    writeFutureMissionsToDom()
+    writeFutureMissionsToDom();
     addFavoriteToList();
   }
 }
@@ -81,11 +80,14 @@ function getFutureLaunches() {
       futureMissions = data;
       // console.log(data);
       // console.log(futureMissions.results);
-    }).then(function () {
+    })
+    .then(function () {
       writeFutureMissionsToDom();
-    }).then(function () {
+    })
+    .then(function () {
       addFavoriteToList();
-    }).then(function () {
+    })
+    .then(function () {
       setInterval(handleLaunchTimers, 1000);
     });
 }
@@ -97,7 +99,6 @@ function writeFutureMissionsToDom() {
   var clearNextFive = document.getElementById("nextFiveLaunchesList");
   clearNextFive.replaceChildren();
   for (let i = 0; i < 90; i++) {
-
     launchTimerArray.push();
     // console.log(futureMissions.results[i].window_start);
     // CARD CONTAINER
@@ -231,7 +232,6 @@ function writeFutureMissionsToDom() {
 }
 
 function addFavoriteToList() {
-
   var favoriteButtons = document.querySelectorAll(".favoriteButtons");
   var clearFuture = document.querySelector(".clearFuture");
   clearFuture.replaceChildren();
@@ -311,7 +311,6 @@ function addFavoriteToList() {
         if (this.textContent == "add") {
           this.textContent = "remove";
           storeUniqueDataID();
-
         } else {
           this.textContent = "add";
           storeUniqueDataID();
@@ -350,17 +349,16 @@ function addFavoriteToList() {
 var launchTimesInSeconds = [];
 
 function handleLaunchTimers() {
-
   var timerDivReady = document.querySelectorAll(".timer-div");
   timerDivReady.textContent = " ";
-  for (i = 0; i < futureMissions['results'].length - 5; i++) {
+  for (i = 0; i < futureMissions["results"].length - 5; i++) {
     launchTimerArray.push(futureMissions.results[i].net);
   }
   for (i = 0; i < 90; i++) {
     var now = moment(),
       end = moment(launchTimerArray[i]),
       secondsUntilLaunch = end.diff(now, "seconds");
-    var theDuration = moment.duration(secondsUntilLaunch, 'seconds');
+    var theDuration = moment.duration(secondsUntilLaunch, "seconds");
     launchTimesInSeconds.push(theDuration);
 
     seconds = theDuration / 1000;
@@ -383,8 +381,6 @@ function handleLaunchTimers() {
     hours = hours.toString().padStart(2, "0");
     days = days.toString().padStart(2, "0");
 
-
-
     var theTime = days + ":" + hours + ":" + minutes + ":" + seconds;
     // console.log(theTime);
 
@@ -400,7 +396,6 @@ function handleLaunchTimers() {
 // -----> Search Lauches page (Itzel's)
 // Itzel's Code BELOW this line -----------------------------------------------------------------------
 
-
 // Global Variables
 var startDate = moment();
 var endDate = moment().add(365, "days");
@@ -411,16 +406,30 @@ var cities = "";
 
 function displayLaunches(response) {
   var results = response.data.results;
+
+  var citiesOptions = [
+    ...new Set(
+      results.map(function (launch) {
+        return launch.pad.location.name;
+      })
+    ),
+  ];
+  var companyOptions = [
+    ...new Set(
+      results.map(function (launch) {
+        return launch.launch_service_provider.name;
+      })
+    ),
+  ];
   var citiesFilterHTML = `<option value="" disabled selected></option>`;
-  for (i = 0; i < results.length; i++) {
-    citiesFilterHTML += `<option>${results[i].pad.location.name}</option>`;
+  for (var i = 0; i < citiesOptions.length; i++) {
+    citiesFilterHTML += `<option>${citiesOptions[i]}</option>`;
   }
 
   var companyFilterHTML = `<option value="" disabled selected></option>`;
-  for (i = 0; i < results.length; i++) {
-    companyFilterHTML += `<option>${results[i].launch_service_provider.name}</option>`;
+  for (var i = 0; i < companyOptions.length; i++) {
+    companyFilterHTML += `<option>${companyOptions[i]}</option>`;
   }
-
   document.querySelector("#company").innerHTML = companyFilterHTML;
   document.querySelector("#cities").innerHTML = citiesFilterHTML;
 
@@ -438,14 +447,6 @@ function displayLaunches(response) {
     searchHTML += launchComponent(results[i]);
   }
   document.querySelector("#searchresults").innerHTML = searchHTML;
-  // Modal listeners
-  for (i = 0; i < results.length; i++) {
-    document.getElementById("search"+results[i].id).addEventListener("click", function () {
-      realid = this.id.split("search")[1];
-      writeModal(realid);
-    });
-  }
-  // Modal listeners
   var saveLaunchHandler = function (event) {
     event.preventDefault();
     var el = event.target;
@@ -462,16 +463,29 @@ function displayLaunches(response) {
     localStorage.setItem("savedMissions", savedMissions);
     addFavoriteToList();
   };
-
   var elements = document.querySelectorAll(".search-add-favorite i");
   for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener("click", saveLaunchHandler);
   }
 
+  // Modal listeners
+  var makeHandler = function displayModalHandler(id) {
+    return function () {
+      writeModal(id);
+    };
+  };
+
+  for (i = 0; i < results.length; i++) {
+    var id = results[i].id;
+    document
+      .querySelector("#search" + id + " .moreBtn")
+      .addEventListener("click", makeHandler(id));
+  }
+  // Modal listeners
+
   for (i = 0; i < results.length; i++) {
     searchHTML += getWeather(results[i]);
   }
-
 }
 
 // Function to search launches inside API
@@ -491,16 +505,18 @@ function launchComponent(launchInfo) {
         <p>Company: <span>${launchInfo.launch_service_provider.name}</span></p>
         <p>Name: <span>${launchInfo.name}</span></p>
         <p>Date: <span>${moment(launchInfo.net).format(
-    "dddd, MMMM Do YYYY, h:mm:ss a"
-  )}</span></p>
+          "dddd, MMMM Do YYYY, h:mm:ss a"
+        )}</span></p>
         <p>Location: <span>${launchInfo.pad.location.name}</span></p>  
       </div>
       <div class="col s5 m3 customWeather">
         <p>Weather: <span class="weather"></span></p>
-      </div> 
+        <button class="moreBtn" id="moreBtn">More</button>
+      </div>  
       <div class="col s1 m1 customIcon">
-        <a href="#" class="saveBtn search-add-favorite"><i data-id="${launchInfo.id
-    }" class="material-icons">
+        <a href="#" class="saveBtn search-add-favorite"><i data-id="${
+          launchInfo.id
+        }" class="material-icons">
         ${savedMissions.indexOf(launchInfo.id) == -1 ? "add" : "remove"}
         </i></a>
       </div>
@@ -548,32 +564,31 @@ function handleFilterSearch(event) {
 var findLaunch = document.querySelector("#findBtn");
 findLaunch.addEventListener("click", handleFilterSearch);
 
-
 // Dawson Code BELOW this line -----------------------------------------------------------------------
 
-
-function writeModal(LaunchID){
+function writeModal(LaunchID) {
   fetch(`https://lldev.thespacedevs.com/2.2.0/launch/${LaunchID}`, {
     method: "GET", //GET is the default.
     credentials: "same-origin", // include, *same-origin, omit
     redirect: "follow", // manual, *follow, error
-  }).then(function (response) {
-    return response.json();
-  }).then(function (data) {
-    mLaunch = data;
-    mTitle = mLaunch.name;
-    mDescription = mLaunch.mission.description;
-    mImage = mLaunch.image;
-    // var mWeather =
-    mTimeDiff = moment(
-      mLaunch.window_start
-    ).fromNow();
-    document.getElementById("modal-title").innerText = mTitle;
-    document.getElementById("modal-desc").innerText = mDescription;
-    document.getElementById("modal-img").src = mImage;
-    // document.getElementById("modal-weather").textContent = mWeather;
-    document.getElementById("modal-tMinus").textContent = "T- " + mTimeDiff;
-  }).then(function () {
-    Dinstance.open();
-  });
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      mLaunch = data;
+      mTitle = mLaunch.name;
+      mDescription = mLaunch.mission.description;
+      mImage = mLaunch.image;
+      // var mWeather =
+      mTimeDiff = moment(mLaunch.window_start).fromNow();
+      document.getElementById("modal-title").innerText = mTitle;
+      document.getElementById("modal-desc").innerText = mDescription;
+      document.getElementById("modal-img").src = mImage;
+      // document.getElementById("modal-weather").textContent = mWeather;
+      document.getElementById("modal-tMinus").textContent = "T- " + mTimeDiff;
+    })
+    .then(function () {
+      Dinstance.open();
+    });
 }
